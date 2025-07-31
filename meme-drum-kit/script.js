31444db01a1cee18eb8650f5bf78e21102486a51
jevitapearl <jevitapearl@gmail.com>
2025-07-31 16:16:41 +0000

    Adding Meme drum kit

---
+++ meme-drum-kit/script.js
@@ -0,0 +1,38 @@
+window.addEventListener("keydown", keySound);
+keyDivs = document.querySelectorAll("div.key");
+
+keyDivs.forEach(div => {
+  div.addEventListener("click", clickSound);
+});
+
+function keySound(e){
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  if(!audio) return;
+
+  key.classList.add("playing");
+  audio.currentTime=0;
+  audio.play();
+}
+
+function clickSound(e){
+  const clickedElement = e.target;
+  const playingKey = clickedElement.getAttribute("data-key");
+  const key = document.querySelector(`.key[data-key="${playingKey}"]`);
+  const audio = document.querySelector(`audio[data-key="${playingKey}"]`);
+
+  if(!audio) return;
+
+  key.classList.add("playing");
+  audio.currentTime=0;
+  audio.play();
+}
+
+const keys = document.querySelectorAll(".key");
+
+keys.forEach(key => key.addEventListener("transitionend", (e) => {
+  if(e.propertyName !== "transform") return;
+
+  e.target.classList.remove("playing");
+}))
